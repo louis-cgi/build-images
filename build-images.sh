79be5ed6c8747b558864645e8592207a4f9e3e0d
Louis C. Tavares <louis.tavares@cgi.com>
2025-02-25 14:37:23 -0500

    init

---
+++ build-images.sh
@@ -0,0 +1,63 @@
+#!/bin/zsh -
+reset-git() {
+  git reset --hard && git clean -dxf
+}
+
+wait-enter() {
+  echo "\nPress any key to continue..."
+  read -s -n 1
+}
+
+ls-images() {
+  echo "\n### Listing Docker Images"
+  docker image ls --format "table {{.Repository}}\t{{.Tag}}\t{{.Size}}"
+  wait-enter
+}
+
+title() {
+  clear
+  echo "###"
+  echo "### $1"
+  echo "###\n"
+}
+
+
+#
+#
+title "Delete Images"
+docker rmi -f app-rust:latest
+docker rmi -f app-sb-compressed:latest
+docker rmi -f app-sb-uncompressed:latest
+docker rmi -f app-sb-jdk:latest
+ls-images
+
+#
+#
+title "Building Java Spring Boot Container with JDK"
+cd ..
+cd multiply-matrix
+reset-git
+git checkout main
+./build.sh
+ls-images
+
+title "Building Java Spring Boot Native (uncompressed)"
+git checkout native-uncompressed
+./build.sh
+ls-images
+
+title "Building Java Spring Boot Native (compressed)"
+git checkout native-compressed
+./build.sh
+ls-images
+
+#
+#
+title "Building Rust Compressed"
+cd ../multiply-rust
+reset-git
+clear
+git checkout main
+./build.sh
+ls-images
+
